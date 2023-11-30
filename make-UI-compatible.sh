eebbac873628728f1285ccfcb5fce5bd66ceb0e0
larizgoitia <larizgoitia@gmail.com>
2023-11-30 12:01:25 +0000

    Update make-UI-compatible.sh (#2)
    Added check for OS version

--- make-UI-compatible.sh
+++ make-UI-compatible.sh
@@ -1,1 +1,11 @@
-sed -i '' 's|\"production\"!==process.env.NODE_ENV|true|g' ./packages/c2pa-wc/dist/components/Popover/popover.js
+#!/bin/bash
+
+os=$(uname -s)
+
+if [ "$os" = "Linux" ]; then
+    sed -i 's|\"production\"!==process.env.NODE_ENV|true|g' ./packages/c2pa-wc/dist/components/Popover/Popover.js
+elif [ "$os" = "Darwin" ]; then
+    sed -i '' 's|\"production\"!==process.env.NODE_ENV|true|g' ./packages/c2pa-wc/dist/components/Popover/Popover.js
+else
+    echo "Unsupported operating system: $os"
+fi
